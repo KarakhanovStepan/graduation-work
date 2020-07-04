5e74eb8a39d06247b5cd684f816ef548481e2d5d
Stepan Karakhanov <stepan.karakhanov@mail.ru>
2020-07-04 17:01:49 +0300

    /api/post - GET - done /api/settings - GET - done

--- src/main/java/ru/home/main/controllers/ApiGeneralController.java
+++ src/main/java/ru/home/main/controllers/ApiGeneralController.java
@@ -1,12 +1,18 @@
 package ru.home.main.controllers;
 
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.home.main.model.GlobalSetting;
+import ru.home.main.model.GlobalSettingRepository;
 
 @RestController
 public class ApiGeneralController
 {
+    @Autowired
+    GlobalSettingRepository globalSettingRepository;
+
     @GetMapping("/api/init")
     public JSONObject init()
     {
@@ -20,5 +26,31 @@ public class ApiGeneralController
         object.put("copyrightFrom", "2020");
 
         return object;
+    }
+
+    @GetMapping("/api/settings")
+    public JSONObject settings()
+    {
+        JSONObject settings = new JSONObject();
+
+        Iterable<GlobalSetting> globalSettings = globalSettingRepository.findAll();
+
+        for(GlobalSetting globalSetting: globalSettings)
+        {
+            if(globalSetting.getCode().equals("MULTIUSER_MODE"))
+            {
+                settings.put("MULTIUSER_MODE", globalSetting.getValue().equals("YES"));
+            }
+            else if(globalSetting.getCode().equals("POST_PREMODERATION"))
+            {
+                settings.put("POST_PREMODERATION", globalSetting.getValue().equals("YES"));
+            }
+            else if(globalSetting.getCode().equals("STATISTICS_IS_PUBLIC"))
+            {
+                settings.put("STATISTICS_IS_PUBLIC", globalSetting.getValue().equals("YES"));
+            }
+        }
+
+        return settings;
     }
 }
--- src/main/java/ru/home/main/controllers/ApiPostController.java
+++ src/main/java/ru/home/main/controllers/ApiPostController.java
@@ -1,5 +1,7 @@
 package ru.home.main.controllers;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.home.main.model.Post;
 import ru.home.main.model.PostModerationStatus;
 import ru.home.main.model.PostRepository;
+import ru.home.main.model.PostVote;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class ApiPostController
@@ -17,7 +23,7 @@ public class ApiPostController
     private PostRepository postRepository;
 
     @GetMapping("/api/post")
-    public List<Post> list(@RequestParam int offset, @RequestParam int limit, @RequestParam String mode)
+    public JSONObject list(@RequestParam int offset, @RequestParam int limit, @RequestParam String mode)
     {
         Iterable<Post> postIterable = postRepository.findAll();
 
@@ -35,15 +41,85 @@ public class ApiPostController
             case ("recent"):
                 list.sort(Comparator.comparing(Post::getTime));
                 break;
-            case ("popular"):
 
+            case ("popular"):
+                list.sort((p1, p2) -> p1.getPostComments().size() > p2.getPostComments().size() ? 1 : 0);
                 break;
+
             case ("best"):
+                list.sort((p1, p2) -> {
+                    int p1Likes = 0;
+
+                    for (PostVote pv: p1.getPostVotes()) {
+                        if(pv.isValue())
+                            p1Likes++;
+                    }
+
+                    int p2Likes = 0;
+
+                    for (PostVote pv: p2.getPostVotes()) {
+                        if(pv.isValue())
+                            p2Likes++;
+                    }
+
+                    return p1Likes > p2Likes ? 1 : 0;
+                });
                 break;
+
             case ("early"):
+                list.sort((p1, p2) -> p1.getTime().after(p2.getTime()) ? 1 : 0);
                 break;
         }
 
-        return list;
+        JSONObject mainObject = new JSONObject();
+
+        mainObject.put("count", list.size());
+
+        JSONArray postsArray = new JSONArray();
+
+        if(offset + limit > list.size())
+            limit = list.size() - offset;
+
+        if(limit != 0) {
+            for (int i = offset; i < offset + limit; i++) {
+
+                Post post = list.get(i);
+                JSONObject postObject = new JSONObject();
+
+                postObject.put("id", post.getId());
+                postObject.put("time", post.getTime());
+
+                JSONObject user = new JSONObject();
+
+                user.put("id", post.getUser().getId());
+                user.put("name", post.getUser().getName());
+
+                postObject.put("user", user);
+
+                postObject.put("title", post.getTitle());
+                postObject.put("announce", post.getText());
+
+                int likes = 0;
+                int dislikes = 0;
+
+                for (PostVote vote : post.getPostVotes()) {
+                    if (vote.isValue())
+                        likes++;
+                    else
+                        dislikes++;
+                }
+
+                postObject.put("likeCount", likes);
+                postObject.put("dislikeCount", dislikes);
+                postObject.put("commentCount", post.getPostComments().size());
+                postObject.put("viewCount", post.getViewCount());
+
+                postsArray.add(postObject);
+            }
+        }
+
+        mainObject.put("posts", postsArray);
+
+        return mainObject;
     }
 }
